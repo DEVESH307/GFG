a92a9dad4ca77c4ee47d8b31b1df962adbcf2ea7
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-26 22:31:26 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -8,29 +8,32 @@ class Solution{
     public:
         //Function to merge the arrays.
         void merge(long long arr1[], long long arr2[], int n, int m) {
-// code here 
-int gap = ceil((float)(n + m) / 2);
-while (gap > 0) {
-int i = 0;
-int j = gap;
-while (j < (n + m)) {
-if (j < n && arr1[i] > arr1[j]) {
-swap(arr1[i], arr1[j]);
-} else if (j >= n && i < n && arr1[i] > arr2[j - n]) {
-swap(arr1[i], arr2[j - n]);
-} else if (j >= n && i >= n && arr2[i - n] > arr2[j - n]) {
-swap(arr2[i - n], arr2[j - n]);
-}
-j++;
-i++;
-}
-if (gap == 1) {
-gap = 0;
-} else {
-gap = ceil((float) gap / 2);
-}
-}
-            } 
+            // code here 
+            int gap = ceil((float)(n + m) / 2);
+            while (gap > 0) {
+                int p1 = 0;
+                int p2 = gap;
+                while (p2 < (n + m)) {
+                    if (p2 < n && arr1[p1] > arr1[p2]) {
+                        swap(arr1[p1], arr1[p2]);
+                    } 
+                    else if (p1 < n && p2 >= n && arr1[p1] > arr2[p2 - n]) {
+                        swap(arr1[p1], arr2[p2 - n]);
+                    } 
+                    else if (p1 >= n && p2 >= n && arr2[p1 - n] > arr2[p2 - n]) {
+                        swap(arr2[p1 - n], arr2[p2 - n]);
+                    }
+                    p1++;
+                    p2++;
+                }
+                if (gap == 1) {
+                    gap = 0;
+                } 
+                else {
+                    gap = ceil((float) gap / 2);
+                }
+            }
+        } 
 };
 
 //{ Driver Code Starts.
