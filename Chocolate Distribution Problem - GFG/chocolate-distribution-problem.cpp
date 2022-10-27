6d1cd9682fe238d4126127c424b10279063aefd3
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-27 22:21:57 +0530

    Added solution - LeetHub

--- Chocolate Distribution Problem - GFG/chocolate-distribution-problem.cpp
+++ Chocolate Distribution Problem - GFG/chocolate-distribution-problem.cpp
@@ -7,37 +7,37 @@ class Solution{
     public:
     long long findMinDiff(vector<long long> arr, long long n, long long m){
     //code
-        // long long minDiff = a[n-1]-a[0];
-        // sort(a.begin(), a.end());
-        // for(int i = 0; i <= n-m; i++){
-        //     minDiff = min(minDiff, a[i+m-1]-a[i]);
-        // }
-        // return minDiff;
-        if (m == 0 || n == 0)
-        return 0;
- 
-        // Sort the given packets
+        long long minDiff = INT_MAX;
         sort(arr.begin(), arr.end());
-     
-        // Number of students cannot be more than
-        // number of packets
-        if (n < m)
-            return -1;
-     
-        // Largest number of chocolates
-        long long min_diff = INT_MAX;
-     
-        // Find the subarray of size m such that
-        // difference between last (maximum in case
-        // of sorted) and first (minimum in case of
-        // sorted) elements of subarray is minimum.
-     
-        for (int i = 0; i + m - 1 < n; i++) {
-            int diff = arr[i + m - 1] - arr[i];
-            if (diff < min_diff)
-                min_diff = diff;
+        for(int i = 0; i <= n-m; i++){
+            minDiff = min(minDiff, arr[i+m-1]-arr[i]);
         }
-        return min_diff;
+        return minDiff;
+        // if (m == 0 || n == 0)
+        // return 0;
+ 
+        // // Sort the given packets
+        // sort(arr.begin(), arr.end());
+     
+        // // Number of students cannot be more than
+        // // number of packets
+        // if (n < m)
+        //     return -1;
+     
+        // // Largest number of chocolates
+        // long long min_diff = INT_MAX;
+     
+        // // Find the subarray of size m such that
+        // // difference between last (maximum in case
+        // // of sorted) and first (minimum in case of
+        // // sorted) elements of subarray is minimum.
+     
+        // for (int i = 0; i + m - 1 < n; i++) {
+        //     int diff = arr[i + m - 1] - arr[i];
+        //     if (diff < min_diff)
+        //         min_diff = diff;
+        // }
+        // return min_diff;
     }   
 };
 
