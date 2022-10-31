040580bc37f6b5033c9c5d637bedc51e3b27dc1c
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-31 16:09:25 +0530

    Added solution - LeetHub

--- Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.cpp
+++ Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.cpp
@@ -13,26 +13,31 @@ class Solution{
     public:
     double MedianOfArrays(vector<int>& array1, vector<int>& array2){
         // Your code goes here
+        if(array1.size() >  array2.size()) return MedianOfArrays(array2, array1);
         int n1 = array1.size();
         int n2 = array2.size();
-        if(n1 >  n2) return MedianOfArrays(array2, array1);
+        
         int low = 0;
         int high = n1;
-        int medPos = (n1+n2+1)/2;
+        int medianPosition = (n1+n2+1)/2;
+        
         while(low <= high){
             int cut1 = (low+high)/2;
-            int cut2 = medPos - cut1;
-            int l1 = (cut1 == 0)? INT_MIN:array1[cut1-1];
-            int l2 = (cut2 == 0)? INT_MIN:array2[cut2-1];
-            int r1 = (cut1 == n1)? INT_MAX:array1[cut1];
-            int r2 = (cut2 == n2)? INT_MAX:array2[cut2];
-            if(l1<=r2 && l2<=r1) {
+            int cut2 = medianPosition - cut1;
+            
+            int left1 = (cut1 == 0)? INT_MIN:array1[cut1-1];
+            int left2 = (cut2 == 0)? INT_MIN:array2[cut2-1];
+            
+            int right1 = (cut1 == n1)? INT_MAX:array1[cut1];
+            int right2 = (cut2 == n2)? INT_MAX:array2[cut2];
+            
+            if(left1<=right2 && left2<=right1) {
                 if((n1+n2)%2 != 0)
-                    return max(l1,l2);
+                    return max(left1,left2);
                 else 
-                    return (max(l1,l2)+min(r1,r2))/2.0;
+                    return (max(left1,left2)+min(right1,right2))/2.0;
             }
-            else if(l1>r2) 
+            else if(left1>right2) 
                 high = cut1-1;
             else 
                 low = cut1+1;
