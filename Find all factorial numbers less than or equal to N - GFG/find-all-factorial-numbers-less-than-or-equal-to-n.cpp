cceb08b2da74d2187a5083433cb4d63fb536cd04
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-01-25 21:22:27 +0530

    Added solution - LeetHub

---
+++ Find all factorial numbers less than or equal to N - GFG/find-all-factorial-numbers-less-than-or-equal-to-n.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    vector<long long> factorialNumbers(long long N){
+        // Write Your Code here
+        vector<long long> res;
+        long long fact = 1;
+        for(long long i = 1; i <= N; i++){
+            fact = fact * i;
+            if(fact > N) break;
+            res.push_back(fact);
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long N;
+        cin>>N;
+        Solution ob;
+        vector<long long> ans = ob.factorialNumbers(N);
+        for(auto num : ans){
+            cout<<num<<" ";
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}
+// } Driver Code Ends
