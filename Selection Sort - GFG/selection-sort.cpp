b93b7c2a75c51d74713e3c3b8f9f00935b3dfc8b
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-01-26 11:43:54 +0530

    Added solution - LeetHub

---
+++ Selection Sort - GFG/selection-sort.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    int select(int arr[], int i){
+        // code here such that selectionSort() sorts arr[]
+        return arr[i];
+    }
+     
+    void selectionSort(int arr[], int n){
+        // //code here
+        // for(int i = 0; i < n-1; i++){
+        //   int minIdx = i;
+        //     for(int j = i+1; j < n; j++){
+        //         if(arr[j] < arr[minIdx]){
+        //             minIdx = j;
+        //         }
+        //     }
+        //   swap(arr[i],arr[minIdx]);
+        // }
+    
+        // Not a stable sort
+        // for(int i = 0; i < n; i++){
+        //       int minIdx = -1;
+        //       int min = INT_MAX;
+        //         for(int j = i; j < n; j++){
+        //             if(arr[j] < min){
+        //                 min = arr[j];
+        //                 minIdx = j;
+        //             }
+        //         }
+        //       swap(arr[i],arr[minIdx]);
+        //     }
+        // }
+    
+        // Stable sort
+        for(int i = 0; i < n-1; i++){
+          int minIdx = i;
+            for(int j = i+1; j < n; j++){
+                if(arr[j] < arr[minIdx]){
+                    minIdx = j;
+                }
+            }
+            int key = arr[minIdx];
+            while(minIdx > i){
+                arr[minIdx] = arr[minIdx-1];
+                minIdx--;
+            }
+            arr[i] = key;
+        }
+    }
+    
+    
+    
+};
+
+//{ Driver Code Starts.
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,T,i;
+  
+    scanf("%d",&T);
+    
+    while(T--){
+        
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      
+    Solution ob;  
+    ob.selectionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
