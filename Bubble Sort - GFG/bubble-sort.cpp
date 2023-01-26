831e5d2ae8e1dd970ec451b941c242566ace9b40
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-01-26 11:56:42 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.cpp
+++ Bubble Sort - GFG/bubble-sort.cpp
@@ -38,8 +38,9 @@ class Solution
         // }
         
         // Recursive solution
+        // First place lasrgest elem at last
+        // and recursively apply bubble sort on remaining elem
         if(n == 1) return;
-        
         int no_of_swap = 0;
         for(int i = 0; i < n-1; i++){
             if(arr[i] > arr[i+1]){
