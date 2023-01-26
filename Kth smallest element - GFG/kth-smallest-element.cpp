796bdb233f7f174a277925b2cf8466172f541fa8
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-01-27 02:14:50 +0530

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.cpp
+++ Kth smallest element - GFG/kth-smallest-element.cpp
@@ -9,6 +9,20 @@ using namespace std;
 
 class Solution{
     public:
+    // Selection Sort Method
+    // int kthSmallest(int arr[], int l, int r, int k){
+    //     for(int i = l; i <= k; i++){
+    //       int minIdx = i;
+    //         for(int j = i+1; j <= r ; j++){
+    //             if(arr[j] < arr[minIdx]){
+    //                 minIdx = j;
+    //             }
+    //         }
+    //       swap(arr[i],arr[minIdx]);
+    //     }
+    //     return arr[k-1];
+    // }
+
     // Binary Search Method
     // int count(int nums[], int r, int& mid){
     //     int cnt = 0;
@@ -74,7 +88,7 @@ class Solution{
     //     return INT_MAX;
     // }
     
-    //Heap Method
+    // Heap Method
     int kthSmallest(int arr[], int l, int r, int k){
         priority_queue<int> pq;
         for(int i = l; i <= r; i++){
