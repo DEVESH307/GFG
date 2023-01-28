8ea8f287a111c2e0e529a7d2696ad8ac4bab99dd
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-01-28 20:53:35 +0530

    Added solution - LeetHub

---
+++ Maximize Number of 1's - GFG/maximize-number-of-1s.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+public:
+    // m is maximum of number zeroes allowed to flip
+    // n is size of array
+    int findZeroes(int arr[], int n, int m) {
+        // code here
+        int ans = 0;
+        int count0 = 0;
+        int j = -1;
+        for(int i = 0; i < n; i++){
+            if(arr[i] == 0){
+                count0++;
+            }
+            while(count0 > m){
+                j++;
+                if(arr[j] == 0){
+                    count0--;
+                }
+            }
+            ans = max(ans, i-j);
+        }
+        return ans;
+    }  
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i, m;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cin >> m;
+        Solution ob;
+        auto ans = ob.findZeroes(arr, n, m);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
