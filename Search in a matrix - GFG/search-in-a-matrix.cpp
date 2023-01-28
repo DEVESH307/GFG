c4c0f91810bbeb8a83200176ae1674309d4dd481
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-01-29 00:21:46 +0530

    Added solution - LeetHub

---
+++ Search in a matrix - GFG/search-in-a-matrix.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int matSearch (vector <vector <int>> &matrix, int n, int m, int target){
+	    // your code here
+        int low = matrix[0][0];
+        int high = matrix[n-1][m-1];
+        
+        if(target < low && target > high) return 0;
+        
+        int i = 0;
+        int j = m-1;
+        while(j >= 0 && i < n){
+            if(matrix[i][j] == target){
+                return true;
+            }
+            else if(matrix[i][j] < target){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return false;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main ()
+{
+    int t; cin >> t;
+    while (t--){
+        int n, m; cin >> n >> m;
+        vector <vector <int>> arr (n, vector <int> (m));
+        for (int i=0;i<n;i++)
+            for (int j=0;j<m;j++)
+                cin >> arr[i][j];
+                
+        int x; cin >> x;
+        Solution ob;
+        cout << ob.matSearch (arr, n, m, x) << endl;
+    }
+}
+// } Driver Code Ends
