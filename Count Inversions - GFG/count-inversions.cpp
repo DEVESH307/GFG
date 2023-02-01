aea061a07a5fa4ecd30742091eb2c9f336f42cdc
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-02-02 00:43:52 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -28,13 +28,13 @@ class Solution{
     // }
     
     // inversion count using merge sort
-    long long int mergeCount(long long int arr[], long long int left,  long long int mid2, long long int right){
-        long long int a = left;
+    long long int mergeCount(long long int arr[], long long int low,  long long int mid2, long long int high){
+        long long int a = low;
         long long int b = mid2;
         long long int c = 0;
         long long int count = 0;
-        long long int temp[right-left+1];
-        while(a < mid2 && b <= right){
+        long long int temp[high-low+1];
+        while(a < mid2 && b <= high){
             if(arr[a] <= arr[b]){
                 temp[c] = arr[a];
                 c++;
@@ -52,24 +52,23 @@ class Solution{
             c++;
             a++;
         }
-        while(b <= right){
+        while(b <= high){
             temp[c] = arr[b];
             c++;
             b++;
         }
-        int idx = 0;
-        for(int i = left; i <= right; i++)
-            arr[i] = temp[idx++];
+        for(int i = low; i <= high; i++)
+            arr[i] = temp[i-low];
         return count;
     }
-    long long int inversionCountHelper(long long int arr[], long long int left,  long long int right){
-        if(left == right){
+    long long int inversionCountHelper(long long int arr[], long long int low,  long long int high){
+        if(low == high){
             return 0;
         }
-        long long int mid = (right+left)/2;
-        long long int x = inversionCountHelper(arr, left, mid);
-        long long int y = inversionCountHelper(arr, mid+1, right);
-        long long int z = mergeCount(arr, left, mid+1, right);
+        long long int mid = (high+low)/2;
+        long long int x = inversionCountHelper(arr, low, mid);
+        long long int y = inversionCountHelper(arr, mid+1, high);
+        long long int z = mergeCount(arr, low, mid+1, high);
         return x+y+z;
     }
     long long int inversionCount(long long int arr[], long long int N){
