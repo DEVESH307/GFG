608a03f6dd7b7567fd284c948b6f13e4acf5e70c
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-02-01 20:55:35 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -4,32 +4,56 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution{
-    public:
-        //Function to merge the arrays.
-        void merge(long long arr1[], long long arr2[], int n, int m) {
-            // code here 
-            int gap = ceil((float)(n + m) / 2);
-            while (gap > 0) {
-                int p1 = 0;
-                int p2 = gap;
-                while (p2 < (n + m)) {
-                    if (p2 < n && arr1[p1] > arr1[p2]) {
-                        swap(arr1[p1], arr1[p2]);
-                    } 
-                    else if (p1 < n && p2 >= n && arr1[p1] > arr2[p2 - n]) {
-                        swap(arr1[p1], arr2[p2 - n]);
-                    } 
-                    else if (p1 >= n && p2 >= n && arr2[p1 - n] > arr2[p2 - n]) {
-                        swap(arr2[p1 - n], arr2[p2 - n]);
-                    }
-                    p1++;
-                    p2++;
-                }
-                gap = gap == 1 ? 0 : ceil((float) gap / 2);
-            }
-        } 
+public:
+    //Function to merge the arrays.
+    void merge(long long arr1[], long long arr2[], int n, int m) {
+        // code here 
+        int arr3[n+m];
+        int k = 0;
+        for(int i = 0; i < n; i++){
+            arr3[k++] = arr1[i];
+        }
+        for(int j = 0; j < m; j++){
+            arr3[k++] = arr2[j];
+        }
+        sort(arr3, arr3+n+m);
+        for(int i = 0; i < n+m; i++){
+            if(i < n)
+                arr1[i] = arr3[i];
+            else
+                arr2[i-n] = arr3[i];
+        }
+    } 
 };
+
+// class Solution{
+// public:
+//     //Function to merge the arrays.
+//     void merge(long long arr1[], long long arr2[], int n, int m) {
+//         // code here 
+//         int gap = ceil((float)(n + m) / 2);
+//         while (gap > 0) {
+//             int p1 = 0;
+//             int p2 = gap;
+//             while (p2 < (n + m)) {
+//                 if (p2 < n && arr1[p1] > arr1[p2]) {
+//                     swap(arr1[p1], arr1[p2]);
+//                 } 
+//                 else if (p1 < n && p2 >= n && arr1[p1] > arr2[p2 - n]) {
+//                     swap(arr1[p1], arr2[p2 - n]);
+//                 } 
+//                 else if (p1 >= n && p2 >= n && arr2[p1 - n] > arr2[p2 - n]) {
+//                     swap(arr2[p1 - n], arr2[p2 - n]);
+//                 }
+//                 p1++;
+//                 p2++;
+//             }
+//             gap = gap == 1 ? 0 : ceil((float) gap / 2);
+//         }
+//     } 
+// };
 
 //{ Driver Code Starts.
 
