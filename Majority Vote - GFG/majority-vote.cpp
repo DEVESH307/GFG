e6d127c0e4424a74bf989cdca4b7cfaf2b25854d
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-02-01 15:46:49 +0530

    Added solution - LeetHub

--- Majority Vote - GFG/majority-vote.cpp
+++ Majority Vote - GFG/majority-vote.cpp
@@ -3,22 +3,75 @@
 using namespace std;
 
 // } Driver Code Ends
+
+// class Solution {
+//   public:
+//     vector<int> Solve(int n, vector<int>& nums) {
+//         // Code here
+//         vector<int> res;
+//         int required_count = n/3;
+//         unordered_map<int, int> mp;
+//         for(auto num: nums){
+//             mp[num]++;
+//         }
+//         for(auto itr: mp){
+//             if(itr.second > required_count){
+//                 res.push_back(itr.first);
+//             }
+//         }
+//         sort(res.begin(), res.end());
+//         if(res.size() == 0){
+//             return {-1};
+//         }
+//         else{
+//             return res;
+//         }
+//     }
+// };
+
 class Solution {
   public:
     vector<int> Solve(int n, vector<int>& nums) {
         // Code here
         vector<int> res;
-        int required_count = n/3;
-        unordered_map<int, int> mp;
-        for(auto num: nums){
-            mp[num]++;
-        }
-        for(auto itr: mp){
-            if(itr.second > required_count){
-                res.push_back(itr.first);
+        int majorityElem1 = -1;
+        int majorityElem2 = -1;
+        int count1 = 0;
+        int count2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == majorityElem1){
+                count1++;
+            }
+            else if(nums[i] == majorityElem2){
+                count2++;
+            }
+            else if(count1 == 0){
+                count1++;
+                majorityElem1 = nums[i];
+            }
+            else if(count2 == 0){
+                count2++;
+                majorityElem2 = nums[i];
+            }
+            
+            else{
+                count1--;
+                count2--;
             }
         }
-        sort(res.begin(), res.end());
+        int countMajorityElem1 = 0;
+        int countMajorityElem2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == majorityElem1){
+                countMajorityElem1++;
+            }
+            else if(nums[i] == majorityElem2){
+                countMajorityElem2++;
+            }
+        }
+        if(countMajorityElem1 > n/3) res.push_back(majorityElem1);
+        if(countMajorityElem2 > n/3) res.push_back(majorityElem2);
+        
         if(res.size() == 0){
             return {-1};
         }
@@ -27,6 +80,7 @@ class Solution {
         }
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
