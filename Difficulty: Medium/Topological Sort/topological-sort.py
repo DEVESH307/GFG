129f68c423cf36202cfd16e53078afb1958bc204
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2026-04-30 01:11:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.py
+++ Difficulty: Medium/Topological Sort/topological-sort.py
@@ -3,14 +3,11 @@ from collections import defaultdict, deque
 class Solution:
     def topoSort(self, V, edges):
         # Code here
-        # build graph
+        # build graph and indegree list
         graph = defaultdict(list)
-        for u, v in edges:
-            graph[u].append(v)
-
-        # build indegree list
         indegree = [0] * (V)
         for u, v in edges:
+            graph[u].append(v)
             indegree[v] += 1
 
         queue = deque([i for i in range(V) if indegree[i] == 0])
